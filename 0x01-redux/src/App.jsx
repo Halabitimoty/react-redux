cf13c54d36d6aac2fe4266241ee1d17c503c9707
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2023-07-14 14:32:38 +0100

    0x02 initial commit and Clean Ups

--- 0x01-redux/src/App.jsx
+++ 0x01-redux/src/App.jsx
@@ -1,12 +1,26 @@
 import "./App.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
   const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch({ type: "INC" });
+  };
+  const decrement = () => {
+    dispatch({ type: "DCR" });
+  };
+  const add = () => {
+    dispatch({ type: "ADD", payload: 10 });
+  };
   return (
     <>
       <h1>Counter App using Deprecated REDUX</h1>
       <h2>{counter}</h2>
+      <button onClick={increment}> + </button>
+      <button onClick={add}> add 10</button>
+      <button onClick={decrement}> - </button>
     </>
   );
 }
