e5209802da66ee7588a73dfbebda5a9c047562e8
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2023-07-29 10:41:31 +0100

    ...

---
+++ 0x03-redux/src/store/cart-slice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    itemsList: [],
+    totalQuantity: 0,
+    showCart: false,
+  },
+  reducers: {
+    addToCart(state, action) {},
+    removeFromCart() {},
+    setShowCart(state) {},
+  },
+});
